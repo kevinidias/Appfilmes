819913354430dafb882f2838539be55b74e181cc
Kevin <kevin.dias.20160@gmail.com>
2021-12-11 19:20:27 -0300

    feat: create project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function appfilmes() {
+ return (
+   <View style={styles.container}>
+
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+});
