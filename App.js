d07f02446998d92edb4666f479591ef0f9a7d8fc
Kevin <kevin.dias.20160@gmail.com>
2021-12-11 19:34:56 -0300

    feat: config api and data.nome

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/Filmes';
 
 export default function appfilmes() {
+
+  const [filmes, setFilmes ] = useState([]);
+
+  useEffect(() => {
+
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes') // pega nossa rota
+      // console.log(response.data)
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+  }, []);
+
  return (
    <View style={styles.container}>
-
+     <FlatList 
+      data={filmes}
+      keyExtractor={ item => String(item.id)}
+      renderItem={ ({ item }) => <Filmes data={item} />}
+     />
    </View>
   );
 }
